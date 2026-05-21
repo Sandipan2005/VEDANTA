96b492cf8132cb1a7728272feb19e2f6db7f0cfe
Sandipan Majumder <sandipan.majumder.2005@outlook.com>
2026-05-22 01:17:24 +0530

    Update FounderSection layout for improved spacing; add right padding to enhance visual consistency.

--- src/pages/Home/FounderSection.jsx
+++ src/pages/Home/FounderSection.jsx
@@ -42,7 +42,7 @@ const FounderSection = () => {
 
         {/* --- FOUNDER SECTION / BOTTOM SECTION --- */}
         <article
-            className="relative z-10 w-full max-w-7xl mx-auto flex flex-col lg:flex-row items-center lg:items-start gap-12 lg:gap-1 bg-[#222222] pt-10 pl-10">
+            className="relative z-10 w-full max-w-7xl mx-auto flex flex-col lg:flex-row items-center lg:items-start gap-12 lg:gap-1 bg-[#222222] pt-10 pl-10 pr-10">
 
             {/* Left Column: Image & Name */}
             <div className="w-full lg:w-4/12 flex flex-col items-center lg:items-start xl:pl-4">
