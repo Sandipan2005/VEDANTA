1eb42ecbf3059b487be42cefaac1f132c1683ae0
Sandipan Majumder <sandipan.majumder.2005@outlook.com>
2026-05-22 17:34:26 +0530

    Update PracticeAreasContent styles for improved layout and visual impact; adjust padding and text shadow for enhanced readability.

--- src/pages/PracticeAreas/PracticeAreasContent.jsx
+++ src/pages/PracticeAreas/PracticeAreasContent.jsx
@@ -49,11 +49,13 @@ const PracticeAreaCard = ({
       {/*</div>*/}
 
       {/* Card Content */}
-      <div className="relative z-10 bg-white p-8 md:p-12 shadow-[0_4px_25px_rgba(0,0,0,0.05)] rounded-sm border border-gray-50 flex flex-col gap-6">
+      <div className="relative z-10 bg-white p-8 md:p-12 lg:pt-6 shadow-[0_4px_25px_rgba(0,0,0,0.05)] rounded-sm border border-gray-50 flex flex-col gap-6">
         <h3
           className="text-2xl md:text-3xl text-center font-bold relative inline-block mx-auto mb-6"
-          style={{ textShadow: " 0px 2px 5px #303030" }}
+          style={{ textShadow: "0px 4px 30px #303030 " }}
+
         >
+            {/*0px 2px 5px #303030,*/}
           {title}
           <div className="absolute left-1/4 right-1/4 h-0.5 bg-[#c4a661] -bottom-3"></div>
         </h3>
