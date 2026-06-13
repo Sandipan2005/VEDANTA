9c4686594fae2864eda237ffe2716b40e29efd88
Sandipan Majumder <sandipan.majumder.2005@outlook.com>
2026-06-13 22:18:13 +0530

    legal disclaimer I have switched the logic from localStorage to sessionStorage.

--- src/components/layout/Disclaimer.jsx
+++ src/components/layout/Disclaimer.jsx
@@ -1,90 +1,56 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Disclaimer.css";
 
-
 const Disclaimer = () => {
+  const [show, setShow] = useState(false);
 
+  useEffect(() => {
+    const hasAgreed = sessionStorage.getItem("agree");
+    if (hasAgreed !== "yes") {
+      setShow(true);
+    }
+  }, []);
 
-  const [show, setShow] = useState(
-    localStorage.getItem("agree") !== "yes"
-  );
-
+  useEffect(() => {
+    if (show) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, [show]);
 
   const agreeClick = () => {
-
-    localStorage.setItem("agree","yes");
-
+    sessionStorage.setItem("agree", "yes");
     setShow(false);
-
   };
 
-
-  if(!show){
+  if (!show) {
     return null;
   }
 
-
-
   return (
-
     <div className="overlay">
-
-
       <div className="disclaimer">
-
-
-        <h2>
-          Legal Disclaimer
-        </h2>
-
-
-
+        <h2>Legal Disclaimer</h2>
         <p>
-
-        As per the rules of the Bar Council of India,
-        we are not permitted to solicit work and advertise.
-        By clicking "I Agree" you confirm the following:
-
+          As per the rules of the Bar Council of India,
+          we are not permitted to solicit work and advertise.
+          By clicking "I Agree" you confirm the following:
         </p>
-
-
-
         <ol>
-
-          <li>
-          No advertisement or solicitation has been made.
-          </li>
-
-
-          <li>
-          You are visiting this website voluntarily.
-          </li>
-
-
-          <li>
-          Information is only for general purposes.
-          </li>
-
-
+          <li>No advertisement or solicitation has been made.</li>
+          <li>You are visiting this website voluntarily.</li>
+          <li>Information is only for general purposes.</li>
         </ol>
-
-
-
         <button onClick={agreeClick}>
           I Agree
         </button>
-
-
-
       </div>
-
-
     </div>
-
   );
-
-
 };
-
 
 export default Disclaimer;
